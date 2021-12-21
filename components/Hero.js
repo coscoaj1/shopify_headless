918b696b361e6b525324cc94a891d75387ada732
coscoaj1 <coscoanthony1@gmail.com>
2021-12-21 06:23:05 -0500

    hero

--- components/Hero.js
+++ components/Hero.js
@@ -54,9 +54,9 @@ export default function Hero({ products }) {
             <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
               <div className="sm:text-center lg:text-left">
                 <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                  <span className="block xl:inline">Data to enrich your</span>{" "}
+                  <span className="block xl:inline"></span> Outdoor
                   <span className="block text-indigo-600 xl:inline">
-                    online business
+                    Gear Zone
                   </span>
                 </h1>
                 <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
@@ -87,11 +87,10 @@ export default function Hero({ products }) {
           </div>
         </div>
         <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-          <Image
+          <img
             className="sm:h-72 md:h-96 lg:w-full lg:h-full"
             src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
             alt=""
-            layout="fill"
           />
         </div>
       </div>
