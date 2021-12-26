0e48a7c692472541ded456e819d6d1fc31f902d6
coscoaj1 <coscoanthony1@gmail.com>
2021-12-26 15:20:04 -0500

    extracted Product List

--- components/Hero.js
+++ components/Hero.js
@@ -4,6 +4,7 @@ import { Fragment } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import backpackImage from "../public/images/fredrik-ohlander-m-Xwt_XF6ks-unsplash.jpg";
+import ProductList from "./ProductList";
 
 export default function Hero({ products }) {
   console.log({ products });
@@ -89,37 +90,7 @@ export default function Hero({ products }) {
           />
         </div>
       </div>
-      <div className="max-w-2xl px-4 py-16 mx-auto sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">
-          Products
-        </h2>
-        <div className="grid grid-cols-1 mt-6 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {products.edges.map((product) => (
-            <div key={product.node.title} className="relative group">
-              <div className="w-full overflow-hidden bg-gray-200 rounded-md min-h-80 aspect-w-4 aspect-h-3 group-hover:opacity-75 lg:h-80 lg:aspect-none">
-                <img
-                  src={product.node.images.edges[0].node.transformedSrc}
-                  className="object-cover object-center w-full h-full lg:w-full lg:h-full"
-                />
-              </div>
-              <div className="flex justify-between mt-4">
-                <div>
-                  <h3 className="text-sm text-gray-700">
-                    <a href={product.href}>
-                      <span aria-hidden="true" className="absolute inset-0" />
-                      {product.node.title}
-                    </a>
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
-                </div>
-                <p className="text-sm font-medium text-gray-900">
-                  {product.node.priceRange.minVariantPrice.amount}
-                </p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
+      <ProductList products={products} />
     </div>
   );
 }
--- components/ProductList.js
+++ components/ProductList.js
@@ -12,6 +12,38 @@ const products = [
   // More products...
 ];
 
-export default function ProductList() {
-  return <div className="bg-white"></div>;
+export default function ProductList({ products }) {
+  return (
+    <div className="max-w-2xl px-4 py-16 mx-auto sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">
+        Products
+      </h2>
+      <div className="grid grid-cols-1 mt-6 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        {products.edges.map((product) => (
+          <div key={product.node.title} className="relative group">
+            <div className="w-full overflow-hidden bg-gray-200 rounded-md min-h-80 aspect-w-4 aspect-h-3 group-hover:opacity-75 lg:h-80 lg:aspect-none">
+              <img
+                src={product.node.images.edges[0].node.transformedSrc}
+                className="object-cover object-center w-full h-full lg:w-full lg:h-full"
+              />
+            </div>
+            <div className="flex justify-between mt-4">
+              <div>
+                <h3 className="text-sm text-gray-700">
+                  <a href={product.href}>
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    {product.node.title}
+                  </a>
+                </h3>
+                <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+              </div>
+              <p className="text-sm font-medium text-gray-900">
+                {product.node.priceRange.minVariantPrice.amount}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
