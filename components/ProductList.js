8ccc48dd6581f3409939406a0a0be97f0f2c4ee9
Anthony Cosco <coscoanthony1@gmail.com>
2022-01-01 10:21:59 -0500

    product

--- components/ProductList.js
+++ components/ProductList.js
@@ -7,35 +7,36 @@ export default function ProductList({ products }) {
 				Products
 			</h2>
 			<div className="grid grid-cols-1 mt-6 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-				{products.edges.map((product) => (
-					<Link key={product.title} href={`/products/${product.node.handle}`}>
-						<a className="relative group">
-							<div>
-								<div className="w-full overflow-hidden bg-gray-200 rounded-md min-h-80 aspect-w-4 aspect-h-3 group-hover:opacity-75 lg:h-80 lg:aspect-none">
-									<img
-										alt={product.node.title}
-										src={product.node.images.edges[0].node.transformedSrc}
-										className="object-cover object-center w-full h-full lg:w-full lg:h-full"
-									/>
-								</div>
-								<div className="flex justify-between mt-4">
-									<div>
-										<h3 className="text-sm text-gray-700">
-											<span aria-hidden="true" className="absolute inset-0" />
-											{product.node.title}
-										</h3>
-										<p className="mt-1 text-sm text-gray-500">
-											{product.color}
+				{products.edges.map((item) => {
+					const product = item.node;
+					const image = product.images.edges[0].node;
+					return (
+						<Link key={product.title} href={`/products/${product.handle}`}>
+							<a className="relative group">
+								<div>
+									<div className="w-full overflow-hidden bg-gray-200 rounded-md min-h-80 aspect-w-4 aspect-h-3 group-hover:opacity-75 lg:h-80 lg:aspect-none">
+										<img
+											alt={image.altText}
+											src={image.transformedSrc}
+											className="object-cover object-center w-full h-full lg:w-full lg:h-full"
+										/>
+									</div>
+									<div className="flex justify-between mt-4">
+										<div>
+											<h3 className="text-sm text-gray-700">
+												<span aria-hidden="true" className="absolute inset-0" />
+												{product.title}
+											</h3>
+										</div>
+										<p className="text-sm font-medium text-gray-900">
+											{product.priceRange.minVariantPrice.amount}
 										</p>
 									</div>
-									<p className="text-sm font-medium text-gray-900">
-										{product.node.priceRange.minVariantPrice.amount}
-									</p>
 								</div>
-							</div>
-						</a>
-					</Link>
-				))}
+							</a>
+						</Link>
+					);
+				})}
 			</div>
 		</div>
 	);
