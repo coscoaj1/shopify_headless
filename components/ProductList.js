9ccea770675891dfee2b38aa72122d2949417ce5
Anthony Cosco <coscoanthony1@gmail.com>
2022-01-01 07:07:45 -0500

    single product page

--- components/ProductList.js
+++ components/ProductList.js
@@ -1,49 +1,95 @@
-const products = [
-  {
-    id: 1,
-    name: "Basic Tee",
-    href: "#",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: "$35",
-    color: "Black",
-  },
-  // More products...
-];
+import { storefront } from '../utils';
 
-export default function ProductList({ products }) {
-  return (
-    <div className="max-w-2xl px-4 py-16 mx-auto sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">
-        Products
-      </h2>
-      <div className="grid grid-cols-1 mt-6 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-        {products.edges.map((product) => (
-          <div key={product.node.title} className="relative group">
-            <div className="w-full overflow-hidden bg-gray-200 rounded-md min-h-80 aspect-w-4 aspect-h-3 group-hover:opacity-75 lg:h-80 lg:aspect-none">
-              <img
-                src={product.node.images.edges[0].node.transformedSrc}
-                className="object-cover object-center w-full h-full lg:w-full lg:h-full"
-              />
-            </div>
-            <div className="flex justify-between mt-4">
-              <div>
-                <h3 className="text-sm text-gray-700">
-                  <a href={product.href}>
-                    <span aria-hidden="true" className="absolute inset-0" />
-                    {product.node.title}
-                  </a>
-                </h3>
-                <p className="mt-1 text-sm text-gray-500">{product.color}</p>
-              </div>
-              <p className="text-sm font-medium text-gray-900">
-                {product.node.priceRange.minVariantPrice.amount}
-              </p>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+export default function ProductList({ products, product: singleProduct }) {
+	console.log({ singleProduct });
+	return (
+		<div className="max-w-2xl px-4 py-16 mx-auto sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+			<h2 className="text-2xl font-extrabold tracking-tight text-gray-900">
+				Products
+			</h2>
+			<div className="grid grid-cols-1 mt-6 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+				{products.edges.map((product) => (
+					<div key={product.node.title} className="relative group">
+						<div className="w-full overflow-hidden bg-gray-200 rounded-md min-h-80 aspect-w-4 aspect-h-3 group-hover:opacity-75 lg:h-80 lg:aspect-none">
+							<img
+								alt={product.node.title}
+								src={product.node.images.edges[0].node.transformedSrc}
+								className="object-cover object-center w-full h-full lg:w-full lg:h-full"
+							/>
+						</div>
+						<div className="flex justify-between mt-4">
+							<div>
+								<h3 className="text-sm text-gray-700">
+									<a href={product.href}>
+										<span aria-hidden="true" className="absolute inset-0" />
+										{product.node.title}
+									</a>
+								</h3>
+								<p className="mt-1 text-sm text-gray-500">{product.color}</p>
+							</div>
+							<p className="text-sm font-medium text-gray-900">
+								{product.node.priceRange.minVariantPrice.amount}
+							</p>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
+
+export async function getStaticPaths() {
+	const { data } = await storefront(gql`
+		{
+			products(first: 4) {
+				edges {
+					node {
+						handle
+					}
+				}
+			}
+		}
+	`);
+	return {
+		paths: data.products.edges.map((product) => ({
+			params: { handle: product.node.handle },
+		})),
+		fallback: false,
+	};
+}
+
+export async function getStaticProps({ params }) {
+	const { data } = await storefront(singleProductQuery, {
+		handle: params.handle,
+	});
+	return {
+		props: {
+			product: data.product,
+		},
+	};
+}
+
+const gql = String.raw;
+const singleProductQuery = gql`
+	query SingleProduct($handle: String!) {
+		product(handle: $handle) {
+			title
+			description
+			updatedAt
+			tags
+			priceRange {
+				minVariantPrice {
+					amount
+				}
+			}
+			images(first: 1) {
+				edges {
+					node {
+						transformedSrc
+						altText
+					}
+				}
+			}
+		}
+	}
+`;
--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,55 @@
-import Head from "next/head";
-import Image from "next/image";
-import Header from "../components/Header";
-import Hero from "../components/Hero";
-import Footer from "../components/Footer";
-import React, { useEffect } from "react";
-import { storefront } from "../utils";
+import Head from 'next/head';
+import Image from 'next/image';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import Footer from '../components/Footer';
+import React, { useEffect } from 'react';
+import { storefront } from '../utils';
 
 export default function Home({ products }) {
-  return (
-    <div className="w-full h-screen">
-      <Header />
-      <Hero products={products} />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="w-full h-screen">
+			<Header />
+			<Hero products={products} />
+			<Footer />
+		</div>
+	);
 }
 
 export async function getStaticProps() {
-  const { data } = await storefront(productsQuery);
-  console.log(data);
-  return {
-    props: {
-      products: data.products,
-    },
-  };
+	const { data } = await storefront(productsQuery);
+	console.log(data);
+	return {
+		props: {
+			products: data.products,
+		},
+	};
 }
 
 const gql = String.raw;
 const productsQuery = gql`
-  query Products {
-    products(first: 4) {
-      edges {
-        node {
-          title
-          handle
-          tags
-          priceRange {
-            minVariantPrice {
-              amount
-            }
-          }
-          images(first: 1) {
-            edges {
-              node {
-                transformedSrc
-                altText
-              }
-            }
-          }
-        }
-      }
-    }
-  }
+	query Products {
+		products(first: 4) {
+			edges {
+				node {
+					title
+					handle
+					tags
+					priceRange {
+						minVariantPrice {
+							amount
+						}
+					}
+					images(first: 1) {
+						edges {
+							node {
+								transformedSrc
+								altText
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 `;
