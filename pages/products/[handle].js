45551fd0a6e401f1cef72ff1b15a4a2f5b9167f4
Anthony Cosco <coscoanthony1@gmail.com>
2022-01-02 11:49:06 -0500

    camel case

--- pages/products/[handle].js
+++ pages/products/[handle].js
@@ -124,7 +124,7 @@ export default function Example({ product, products }) {
 											cy="12"
 											r="10"
 											stroke="currentColor"
-											stroke-width="4"
+											strokeWidth="4"
 										></circle>
 										<path
 											className="opacity-75"
