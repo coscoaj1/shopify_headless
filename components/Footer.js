e96ba7de63e4423a6f6a0cebc9e4248026102c0f
coscoaj1 <coscoanthony1@gmail.com>
2022-01-20 17:12:13 -0500

    updated header spacing and footer text

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import { FaFacebook, FaInstagramSquare, FaTwitter } from "react-icons/fa";
 function Footer() {
   return (
     <footer className="flex justify-between p-4 mx-auto border-t border-gray-300 max-w-7xl">
-      <p className="text-gray-700">Copyright 2021 Gear Shop</p>
+      <p className="text-gray-700">Copyright 2022 Vital Gear Shop</p>
       <div className="flex gap-4">
         <FaFacebook className="w-6 h-6 text-gray-700" />
         <FaInstagramSquare className="w-6 h-6 text-gray-700" />
